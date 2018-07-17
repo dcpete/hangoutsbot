006dea3394d0c5f85405f602157990e4493b1997
dcp <dcp@dcpete.io>
2018-07-17 01:14:57 -0400

    Added what-defines-a-haha embodiment check and made it available to other plugins

--- hangupsbot/plugins/haha.py
+++ hangupsbot/plugins/haha.py
@@ -9,11 +9,44 @@ logger = logging.getLogger(__name__)
 def _initialise(bot):
     plugins.register_handler(_handle_haha_message, "message")
     plugins.register_user_command(["hahas", "scoreboard"])
+    bot.register_shared("haha.is_haha", is_haha)
+
+
+def is_legal_haha_characters(text):
+    hCount = 0
+    aCount = 0
+    tot = len(text)
+    logger.debug("haha length: {}".format(tot))
+    ratio = 0.4
+    for c in text.lower():
+        logger.debug('letter: {}'.format(c))
+        if c != 'h' and c != 'a': 
+            logger.debug("illegal letter {}, quitting".format(c))
+            return False
+        if c == 'h': 
+            hCount = hCount + 1
+            logger.debug("h, count: {}".format(hCount))
+        if c == 'a': 
+            aCount = aCount + 1
+            logger.debug("a, count: {}".format(aCount))
+    return hCount/tot >= ratio and aCount/tot >= ratio
+
+
+def is_haha(text):
+    # Looking for 
+    #  One continuous word
+    #  At least 4 characters long
+    #  Only contains h or a
+    logger.debug("word is: {}".format(text))
+    logger.debug("number of words: {}".format(len(text.split())))
+    logger.debug("number of letters: {}".format(len(text)))
+    logger.debug("is legal_haha: {}".format(is_legal_haha_characters(text)))
+    return len(text.split()) == 1 and len(text) > 3 and is_legal_haha_characters(text)
 
 
 def _handle_haha_message(bot, event, command):
     """handle mesages that are haha"""
-    if event.text == "haha":
+    if is_haha(event.text):
         logger.debug("haha message")
         last_not_haha = bot.conversation_memory_get(event.conv_id, "last_not_haha_user_id")
         if event.user_id.chat_id != last_not_haha:
@@ -24,7 +57,7 @@ def _handle_haha_message(bot, event, command):
             bot.user_memory_set(last_not_haha, "haha_count", n + 1)
             logger.debug("added haha for {}".format(last_not_haha))
         else:
-            message = "Nice try, {}".format(event.user.full_name)
+            message = "Nice try, {}".format(event.user.full_name.split()[1:])
             yield from bot.coro_send_message(event.conv_id, message)
     else:
         logger.debug("not haha message")
